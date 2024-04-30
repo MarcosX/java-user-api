e177ef5007cd76171abad161fd60310ec063fe60
ingridmidia <ingridmidia@gmail.com>
2024-04-30 17:39:48 -0500

    check encoded password

--- src/main/java/com/maggie/userapi/controllers/LoginController.java
+++ src/main/java/com/maggie/userapi/controllers/LoginController.java
@@ -4,6 +4,7 @@ import com.maggie.userapi.models.User;
 import com.maggie.userapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,7 +32,9 @@ public class LoginController {
 
         User user = userRepository.findByUsername(loginForm.username());
 
-        if (user != null && loginForm.password().equals(user.getPassword())) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        
+        if (user != null && encoder.matches(loginForm.password(), user.getPassword())) {
             return new LoginResponse("token");
         }
         throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
