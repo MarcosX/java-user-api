0fec537b99996a7e92b0786f5d4c1d1888ecf8cb
ingridmidia <ingridmidia@gmail.com>
2024-05-01 16:05:40 -0500

    validate email

--- src/main/java/com/maggie/userapi/controllers/SignupController.java
+++ src/main/java/com/maggie/userapi/controllers/SignupController.java
@@ -3,11 +3,15 @@ package com.maggie.userapi.controllers;
 import com.maggie.userapi.models.User;
 import com.maggie.userapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.regex.Pattern;
 
 record SignupForm(String username, String email, String password) {
 }
@@ -22,9 +26,20 @@ public class SignupController {
 
     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
+    Pattern emailPattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
     @PostMapping("/signup")
     @ResponseBody
     public SignupResponse signup(@RequestBody SignupForm signupForm) {
+        if (userRepository.findByEmail(signupForm.email()) != null) {
+            // Email already being used
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        
+        if (!emailPattern.matcher(signupForm.email()).matches()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         String password = encoder.encode(signupForm.password());
         User newUser = userRepository.save(new User(signupForm.username(), signupForm.email(), password));
         return new SignupResponse("token", newUser);
