3db6b9dda0e7b861f570e705ac4d751474ed45b0
ingridmidia <ingridmidia@gmail.com>
2024-05-04 16:46:52 -0500

    add user validation

--- src/main/java/com/maggie/userapi/controllers/SignupController.java
+++ src/main/java/com/maggie/userapi/controllers/SignupController.java
@@ -5,14 +5,11 @@ import com.maggie.userapi.repositories.UserRepository;
 import com.maggie.userapi.session.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.regex.Pattern;
 
 record SignupForm(String username, String email, String password) {
 }
@@ -28,37 +25,21 @@ public class SignupController {
     @Autowired
     JwtService jwtService;
 
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-    Pattern emailPattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-
-    Pattern passwordPattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$");
-
     @PostMapping("/signup")
     @ResponseBody
     public SignupResponse signup(@RequestBody SignupForm signupForm) {
         if (userRepository.findByEmail(signupForm.email()) != null) {
-            // Email already being used
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already being used.");
         }
 
-        if (!emailPattern.matcher(signupForm.email()).matches()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid email format.");
+        try {
+            User newUser = userRepository.save(new User(signupForm.username(), signupForm.email(), signupForm.password()));
+
+            String token = jwtService.generateToken(newUser);
+
+            return new SignupResponse(token, newUser.getUsername());
+        } catch (IllegalArgumentException exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage());
         }
-
-        if (!passwordPattern.matcher(signupForm.password()).matches()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password must have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.");
-        }
-
-        if (signupForm.username().trim().length() < 2) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username must have at least 2 characters.");
-        }
-
-        String password = encoder.encode(signupForm.password());
-        User newUser = userRepository.save(new User(signupForm.username(), signupForm.email(), password));
-
-        String token = jwtService.generateToken(newUser);
-
-        return new SignupResponse(token, newUser.getUsername());
     }
 }
--- src/main/java/com/maggie/userapi/models/User.java
+++ src/main/java/com/maggie/userapi/models/User.java
@@ -2,6 +2,9 @@ package com.maggie.userapi.models;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.regex.Pattern;
 
 @Document("Users")
 public class User {
@@ -11,11 +14,33 @@ public class User {
     private String username;
     private String email;
     private String password;
+    
+    static final String USERNAME_ERROR_MESSAGE = "Username must have at least 2 characters.";
+    static final String PASSWORD_ERROR_MESSAGE = "Password must have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.";
+    static final String EMAIL_ERROR_MESSAGE = "Invalid email format.";
+
 
     public User(String username, String email, String password) {
+        if (username.trim().length() < 2) {
+            throw new IllegalArgumentException(USERNAME_ERROR_MESSAGE);
+        }
+
+        Pattern emailPattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        if (!emailPattern.matcher(email).matches()) {
+            throw new IllegalArgumentException(EMAIL_ERROR_MESSAGE);
+        }
+
+        Pattern passwordPattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$");
+        if (!passwordPattern.matcher(password).matches()) {
+            throw new IllegalArgumentException(PASSWORD_ERROR_MESSAGE);
+        }
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(password);
+
         this.username = username;
         this.email = email;
-        this.password = password;
+        this.password = encodedPassword;
     }
 
     public String getUsername() {
